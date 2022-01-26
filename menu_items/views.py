6ef8076a0c95313d290e861fab17f64997843fe1
Mr Alizadeh <mhalizadeh180@gmail.com>
2022-01-26 15:50:30 +0330

    Add: MenuItemsList View

--- menu_items/views.py
+++ menu_items/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Category, MenuItem
+
 
 # Create your views here.
+
+
+class MenuItemsList(ListView):
+    model = MenuItem
+    template_name = 'menu_items/menu_item/sample.html'
