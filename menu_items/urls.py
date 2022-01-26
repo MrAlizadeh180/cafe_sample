f1817de95d6eeeeaa5ba3bdc8492fe5cd38ad17b
Mr Alizadeh <mhalizadeh180@gmail.com>
2022-01-26 17:34:33 +0330

    reformat: category/sample

--- menu_items/urls.py
+++ menu_items/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('menu_items/', MenuItemsList.as_view(), name='menu_items_list')
+    path('menu_items/', MenuItemsList.as_view(), name='menu_items_list'),
+    path('categories/', CategoryList.as_view(), name="categories")
 ]
