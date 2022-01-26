925ec891b9f1b90afd9e7854b08dc7256929efdb
Mr Alizadeh <mhalizadeh180@gmail.com>
2022-01-26 22:12:15 +0330

    Customize Admin panel

--- orders/views.py
+++ orders/views.py
@@ -1,15 +1,38 @@
 from django.shortcuts import render
 from django.views import generic
 # Create your views here.
-from .models import Order
+from .models import Order, OrderItem
+from tables.models import Table
 
 
 class CrateOrder(generic.edit.CreateView):
     model = Order
     fields = ['table']
 
+    # def get(self, request, *args, **kwargs):
+    #     kwargs.update({'item': 'lkjlk'})
+    #     print(kwargs)
+    #     return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        table = Table.objects.get(id=int(request.POST.get('table')))
+        # order = Order.objects.last()
+        # asd = OrderItem(order=request.POST)
+        return super().post(request, *args, **kwargs)
+        # asd.save()
+
 
 class OrderDetail(generic.DetailView):
     model = Order
     template_name = 'orders/order_detail.html'
 
+
+# class OrderICreate(generic.CreateView):
+#     model = OrderItem
+#     fields = ['item', 'quantity', 'nnnn']
+#     template_name = 'orders/asd.html'
+#
+#     def post(self, request, *args, **kwargs):
+#         order = Order.objects.get(id=1)
+#         kwargs.update({'order': order})
+#         return super().post(request, *args, **kwargs)
